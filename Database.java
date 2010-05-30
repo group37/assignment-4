836e7e5f74ce6b889205c0fba825994ad42b0472
ben <cs345-group-37-gg@aucklanduni.ac.nz>
2010-05-30 23:54:00 +1200

    test commit
    Signed-off-by: ben <cs345-group-37-gg@aucklanduni.ac.nz>

--- Database.java
+++ Database.java
@@ -1,6 +1,7 @@
 // stuff to put in test data for teams
 // array of teams?
 //import src.Team;
+//
 public class Database {
 	public enum Division { JUNIOR, ADULT_BEGINNER, ADULT_INTERMEDIATE, ADULT_ADVANCED, SENIOR }
 	Team[] teams = new Team[10];
